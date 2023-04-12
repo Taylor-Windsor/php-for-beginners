9674924d7528ab326cc8955b25550acfa279c93f
Taylor Windsor <taylorjwindsor@gmail.com>
2023-04-12 14:14:50 -0400

    finished through Programming is Rewriting section

--- dynamic-web/index.php
+++ dynamic-web/index.php
@@ -1,17 +1,18 @@
 <?php
 require 'functions.php';
-// require 'router.php';
 require 'Database.php';
+require 'Response.php';
+require 'router.php';
 
-$config = require('config.php');
-$db = new Database($config['database']);
 
-$id = $_GET['id'];
-$query = "select * from posts where id = :id";
 
-$posts = $db->query($query, ['id' => $id])->fetch();
 
-dd($posts);
+// $id = $_GET['id'];
+// $query = "select * from posts where id = :id";
+
+// $posts = $db->query($query, ['id' => $id])->fetch();
+
+// dd($posts);
 // foreach ($posts as $post) {
 //     echo "<li>" . $post['title'] . "</li>";
 // }
