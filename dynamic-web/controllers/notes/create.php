72f67edb5164dc59bf278500c6f4ffc8f280596e
Taylor Windsor <taylorjwindsor@gmail.com>
2023-04-14 10:27:33 -0400

    finished through Handle Multiple Request Methods From a Controller Action Section

--- dynamic-web/controllers/notes/create.php
+++ dynamic-web/controllers/notes/create.php
@@ -1,12 +1,13 @@
 <?php
-require 'Validator.php';
-$config = require('config.php');
+use Core\Database;
+use Core\Validator;
+$config = require base_path('config.php');
 $db = new Database($config['database']);
-$heading = 'Create Note';
+$errors = [];
 
 
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-    $errors = [];
+    
     if (!Validator::string($_POST['body'],1,1000)) {
         $errors['body'] = 'A body of no more than 1,000 characters is required';
     }
@@ -19,4 +20,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
         ]);
     }
 }
-require 'views/note-create.view.php';
+view("notes/create.view.php", [
+    'heading' => 'Create Note',
+    'errors' => $errors
+]);
--- dynamic-web/routes.php
+++ dynamic-web/routes.php
@@ -2,9 +2,9 @@
 return [
     '/' => 'controllers/index.php',
     '/about' => 'controllers/about.php',
-    '/notes' => 'controllers/notes.php',
-    '/note' => 'controllers/note.php',
+    '/notes' => 'controllers/notes/index.php',
+    '/note' => 'controllers/notes/show.php',
     '/contact' => 'controllers/contact.php',
-    '/notes/create' => 'controllers/note-create.php'
+    '/notes/create' => 'controllers/notes/create.php'
 
 ];
